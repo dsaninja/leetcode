d8767261713fc3f6f72b5cc8aa9570ef6072d1ab
dsaninja <dsaninjablog@gmail.com>
2022-04-12 22:36:09 +0530

    updated for GameOfLife

--- src/main/java/com/dsaninja/lc/dailychallenge/GameOfLife.java
+++ src/main/java/com/dsaninja/lc/dailychallenge/GameOfLife.java
@@ -21,45 +21,105 @@ package com.dsaninja.lc.dailychallenge;
  * <p>
  * https://leetcode.com/problems/game-of-life/
  */
-public class GameOfLife {
+public class GameOfLife{
 
     // time complexity : O(mn)
     // space complexity : O(mn)
-    public void gameOfLife(int[][] board) {
+    public void gameOfLife(int[][] board){
         int rows = board.length;
         int cols = board[0].length;
 
         int[][] clone = new int[rows][cols];
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
                 clone[i][j] = board[i][j];
             }
         }
 
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
+        // Iterate through board cell by cell
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
 
+                // for each [row,col], calculate its live neighbours
                 int liveNeighbours = 0;
 
+                // calculate bounds for [row,col] pair
+                // for (0,0), top = 0, bottom = 1, left = 0, right = 1
                 int top = Math.max(0, row - 1);
                 int bottom = Math.min(rows - 1, row + 1);
-                int left = Math.min(0, col - 1);
+                int left = Math.max(0, col - 1);
                 int right = Math.min(cols - 1, col + 1);
 
-                for (int i = top; i <= bottom; i++) {
-                    for (int j = left; j <= right; j++) {
+                // <= operator to include diagonals
+                for(int i = top; i <= bottom; i++){
+                    for(int j = left; j <= right; j++){
                         if(!(i == row && j == col) && clone[i][j] == 1){
-                             liveNeighbours++;
+                            liveNeighbours++;
                         }
                     }
                 }
 
                 if(liveNeighbours < 2 || liveNeighbours > 3){
+                    // originally live but now dead
                     board[row][col] = 0;
                 } else if(liveNeighbours == 3){
+                    // originally dead but now alive
                     board[row][col] = 1;
                 }
+            }
+        }
+    }
+
+    // time complexity : O(mn)
+    public void gameOfLifeInPlace(int[][] board){
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Iterate through board cell by cell
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+
+                // for each [row,col], calculate its live neighbours
+                int liveNeighbours = 0;
+
+                // calculate bounds for [row,col] pair
+                // for (0,0), top = 0, bottom = 1, left = 0, right = 1
+                int top = Math.max(0, row - 1);
+                int bottom = Math.min(rows - 1, row + 1);
+                int left = Math.max(0, col - 1);
+                int right = Math.min(cols - 1, col + 1);
+
+                // <= operator to include diagonals
+                for(int i = top; i <= bottom; i++){
+                    for(int j = left; j <= right; j++){
+                        if(!(i == row && j == col) && Math.abs(board[i][j]) == 1){
+                            liveNeighbours++;
+                        }
+                    }
+                }
+
+                // for a live cell
+                if(board[row][col] == 1){
+                    if(liveNeighbours < 2 || liveNeighbours > 3){
+                        // originally live but now dead
+                        board[row][col] = -1;
+                    } else if(liveNeighbours == 3){
+                        board[row][col] = 1;
+                    }
+                }else{
+                    if(liveNeighbours == 3){
+                        // originally dead but now alive
+                        board[row][col] = 2;
+                    }
+                }
+            }
+        }
+
+        // update -1 and 2 to 0 and 1
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                board[row][col] = board[row][col] > 0 ? 1 : 0;
             }
         }
     }
