26b886767b08c3085784f15b08252faa9ef72681
dsaninja <dsaninjablog@gmail.com>
2022-04-18 22:13:08 +0530

    updated for a few searching and sorting problems

---
+++ src/main/java/com/dsaninja/lc/tiq/easy/sands/FirstBadVersion.java
@@ -0,0 +1,40 @@
+package com.dsaninja.lc.tiq.easy.sands;
+
+/**
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check. Since
+ * each version is developed based on the previous version, all the versions after a
+ * bad version are also bad.
+ * <p>
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+ * which causes all the following ones to be bad.
+ * <p>
+ * You are given an API bool isBadVersion(version) which returns whether version is bad.
+ * Implement a function to find the first bad version. You should minimize the number of
+ * calls to the API.
+ * <p>
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/774/
+ */
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        int result = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (isBadVersion(mid)) {
+                result = mid;
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return result;
+    }
+
+    // given implementation
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
+}
---
+++ src/main/java/com/dsaninja/lc/tiq/easy/sands/MergeSortedArrays.java
@@ -0,0 +1,50 @@
+package com.dsaninja.lc.tiq.easy.sands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+ * and two integers m and n, representing the number of elements in nums1 and nums2
+ * respectively.
+ * <p>
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * <p>
+ * The final sorted array should not be returned by the function, but instead be
+ * stored inside the array nums1. To accommodate this, nums1 has a length of m + n,
+ * where the first m elements denote the elements that should be merged, and the
+ * last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * <p>
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/587/
+ */
+public class MergeSortedArrays {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length -1;
+
+        // i and j  are pointing to respective array ends
+        int i = m - 1, j = n - 1;
+
+        // start from right till beginning of array
+        while (index >= 0){
+            if(j < 0){
+                break;
+            }
+
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[index--] = nums1[i--];
+            } else {
+                nums1[index--] = nums2[j--];
+            }
+        }
+    }
+
+    @Test
+    public void testSortedMerge() {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+
+        assertArrayEquals(new int[]{1, 2, 2, 3, 5, 6}, nums1);
+    }
+}
